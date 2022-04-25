0aceee82afaec5266029f1dda3b44db9c26627c4
harshrajsaxena <harshraj.saxena.18@gmail.com>
2022-04-25 14:44:59 +0530

    Add files via upload

---
+++ simintrest.py
@@ -0,0 +1,6 @@
+p=float(input("enter the principal amount="))
+t=int(input("enter the time in year="))
+r=float(input("enter the rate ="))
+sim=(p*t*r)/100
+print("simpil intrest on value of {} is {}".format(p,sim))
+
