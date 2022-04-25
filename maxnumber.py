58661dee7798e3078e72d7646eae87c24ae47533
harsh saxena <harshraj.saxena.18@gmail.com>
2022-04-25 13:35:02 +0530

    maxnumber.py

---
+++ maxnumber.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 13:07:19 2022
+
+@author: Saxena_Harsh
+"""
+
+a=int(input("enter first number"))
+b=int(input("enter secound number"))
+if a>=b:
+    print("{} is maximum number".format(a))
+else:
+    print("{} is maximum number".format(b))
